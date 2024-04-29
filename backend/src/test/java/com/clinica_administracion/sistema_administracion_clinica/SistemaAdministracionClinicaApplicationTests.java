fa3df5a7f785b6018f03ca5505a10d99c5a66c45
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-04-29 12:05:48 -0300

    starting proyect and backend

---
+++ backend/src/test/java/com/clinica_administracion/sistema_administracion_clinica/SistemaAdministracionClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica_administracion.sistema_administracion_clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAdministracionClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
