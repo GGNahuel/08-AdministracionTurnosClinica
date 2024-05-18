60b94d87cf82cbbd41b1597796cbaba60507172e
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-05-18 01:39:42 -0300

    localTime to string in turnoDTO

--- backend/src/main/java/com/clinica_administracion/sistema_administracion_clinica/DTOs/TurnoDTO.java
+++ backend/src/main/java/com/clinica_administracion/sistema_administracion_clinica/DTOs/TurnoDTO.java
@@ -1,6 +1,5 @@
 package com.clinica_administracion.sistema_administracion_clinica.DTOs;
 
-import java.time.LocalTime;
 import java.util.UUID;
 
 import com.clinica_administracion.sistema_administracion_clinica.others.enums.EstadoPago;
@@ -16,7 +15,7 @@ public class TurnoDTO {
   private UUID id;
   private String pacienteDni;
   private String fecha;
-  private LocalTime horario;
+  private String horario;
   private String areaProfesional;
   private String metodoDeAbono;
   private String obraSocial;
