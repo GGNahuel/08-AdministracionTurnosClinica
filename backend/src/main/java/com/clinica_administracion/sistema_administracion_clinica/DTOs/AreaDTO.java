b07d310e1240ce47b0a25293fe4f65b018446248
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-05-28 23:48:29 -0300

    adding builder

--- backend/src/main/java/com/clinica_administracion/sistema_administracion_clinica/DTOs/AreaDTO.java
+++ backend/src/main/java/com/clinica_administracion/sistema_administracion_clinica/DTOs/AreaDTO.java
@@ -2,9 +2,11 @@ package com.clinica_administracion.sistema_administracion_clinica.DTOs;
 
 import java.util.UUID;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AreaDTO {
   private UUID id;
   private String nombre;
--- backend/src/main/java/com/clinica_administracion/sistema_administracion_clinica/entities/AreaEntity.java
+++ backend/src/main/java/com/clinica_administracion/sistema_administracion_clinica/entities/AreaEntity.java
@@ -6,10 +6,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AreaEntity {
   @Id @GeneratedValue
   private UUID id;
