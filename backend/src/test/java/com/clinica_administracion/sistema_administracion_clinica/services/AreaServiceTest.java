680e04a25d9b29c4c88e008164085fd5027e1f71
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-05-29 22:15:09 -0300

    getAll & getByActiveState tests

--- backend/src/test/java/com/clinica_administracion/sistema_administracion_clinica/services/AreaServiceTest.java
+++ backend/src/test/java/com/clinica_administracion/sistema_administracion_clinica/services/AreaServiceTest.java
@@ -1,8 +1,12 @@
 package com.clinica_administracion.sistema_administracion_clinica.services;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.clinica_administracion.sistema_administracion_clinica.DTOs.AreaDTO;
 import com.clinica_administracion.sistema_administracion_clinica.entities.AreaEntity;
+import com.clinica_administracion.sistema_administracion_clinica.others.exceptions.NotNullFieldIsNull;
 import com.clinica_administracion.sistema_administracion_clinica.repositories.AreaRepository;
 
 @SpringBootTest
@@ -32,6 +37,8 @@ public class AreaServiceTest {
   private AreaDTO areaDto1;
   private AreaDTO areaDto2;
   private AreaDTO areaDto3;
+
+  private List<AreaDTO> expectedDtoList = new ArrayList<>();
 
   @BeforeEach
   public void setUp() {
@@ -73,19 +80,92 @@ public class AreaServiceTest {
       .necesitaTurno(false)
     .build();
 
+    /* AreaDTO areafalse = AreaDTO.builder()
+    .id(areaEntity3.getId())
+    .nombre("Qwerty")
+    .activa(true)
+    .necesitaTurno(false)
+    .build(); */
+    
+    expectedDtoList.clear();
+
     when(modelMapper.map(areaEntity1, AreaDTO.class)).thenReturn(areaDto1);
     when(modelMapper.map(areaEntity2, AreaDTO.class)).thenReturn(areaDto2);
     when(modelMapper.map(areaEntity3, AreaDTO.class)).thenReturn(areaDto3);
   }
-
+  
   @Test
-  public void areaService_getAll_returnsAllInDto() {
-    List<AreaEntity> lista = List.of(areaEntity1, areaEntity2, areaEntity3);
-    when(areaRepo.findAll()).thenReturn(lista);
+  public void areaService_getAll_returnsExpectedDtosInAList() {
+    expectedDtoList.add(areaDto1);
+    expectedDtoList.add(areaDto2);
+    expectedDtoList.add(areaDto3);
+    List<AreaEntity> entitiesList = List.of(areaEntity1, areaEntity2, areaEntity3);
+    when(areaRepo.findAll()).thenReturn(entitiesList);
 
     List<AreaDTO> listaDto = areaService.getAll();
 
-    assertEquals(3, listaDto.size());
-    Assertions.assertThat(listaDto).containsExactlyInAnyOrder(areaDto1, areaDto2, areaDto3);
+    assertEquals(3, listaDto.size(), "El tamaño de las listas deberían ser iguales");
+    Assertions.assertThat(listaDto)
+      .withFailMessage("La lista obtenida debería retornar los dtos esperados: <%s>, <%s>, <%s>", 
+        areaDto1.toString(), areaDto2.toString(), areaDto3.toString()
+      )
+    .containsExactlyInAnyOrder(areaDto1, areaDto2, areaDto3);
+    assertAll("Los dtos obtenidos deberían ser los mismos que se obtienen de las entidades", 
+      () -> assertTrue(areaDto1.equals(listaDto.get(0)), 
+        "El área dto en el índice 0 no es igual al esperado: " + areaDto1.toString() + " - " + listaDto.get(0).toString()),
+      () -> assertTrue(areaDto2.equals(listaDto.get(1)), 
+        "El área dto en el índice 1 no es igual al esperado: " + areaDto2.toString() + " - " + listaDto.get(1).toString()),
+      () -> assertTrue(areaDto3.equals(listaDto.get(2)), 
+        "El área dto en el índice 2 no es igual al esperado: " + areaDto3.toString() + " - " + listaDto.get(2).toString())
+    );
   }
+
+  @Test
+  public void areaService_getByActiveState_returnActivesAreasDtos() throws Exception {
+    expectedDtoList.add(areaDto1);
+    expectedDtoList.add(areaDto3);
+    List<AreaEntity> entitiesList = List.of(areaEntity1, areaEntity3);
+    when(areaRepo.findByActiva(true)).thenReturn(entitiesList);
+
+    List<AreaDTO> listaDto = areaService.getByActiveState(true);
+
+    assertEquals(2, listaDto.size(), "El tamaño de las listas deberían ser iguales");
+    Assertions.assertThat(listaDto)
+      .withFailMessage("La lista obtenida debería retornar los dtos esperados: <%s>, <%s>", 
+        areaDto1.toString(), areaDto3.toString()
+      )
+    .containsExactlyInAnyOrder(areaDto1, areaDto3);
+    assertAll("Los dtos obtenidos deberían ser los mismos que se obtienen de las entidades", 
+      () -> assertTrue(areaDto1.equals(listaDto.get(0)), 
+        "El área dto en el índice 0 no es igual al esperado: " + areaDto1.toString() + " - " + listaDto.get(0).toString()),
+      () -> assertTrue(areaDto3.equals(listaDto.get(1)), 
+        "El área dto en el índice 1 no es igual al esperado: " + areaDto3.toString() + " - " + listaDto.get(1).toString())
+    );
+  }
+
+  @Test
+  public void areaService_getByActiveState_returnNotActivesAreasDtos() throws Exception {
+    expectedDtoList.add(areaDto2);
+    List<AreaEntity> entitiesList = List.of(areaEntity2);
+    when(areaRepo.findByActiva(false)).thenReturn(entitiesList);
+
+    List<AreaDTO> listaDto = areaService.getByActiveState(false);
+
+    assertEquals(1, listaDto.size(), "El tamaño de las listas deberían ser iguales");
+    Assertions.assertThat(listaDto)
+      .withFailMessage("La lista obtenida debería retornar el dto esperado: <%s>", areaDto2.toString())
+    .containsExactlyInAnyOrder(areaDto2);
+    assertAll("Los dtos obtenidos deberían ser los mismos que se obtienen de las entidades", 
+      () -> assertTrue(areaDto2.equals(listaDto.get(0)), 
+        "El área dto en el índice 0 no es igual al esperado: " + areaDto2.toString() + " - " + listaDto.get(0).toString())
+    );
+  }
+
+  @Test
+  public void areaService_getByActiveState_throwsNoNullFieldException() {
+    assertThrows(NotNullFieldIsNull.class, () -> areaService.getByActiveState(null), 
+      "La función debería arrojar una excepción NotNullFieldIsNull cuando se ingrese un valor nulo");
+  }
+
+  
 }
