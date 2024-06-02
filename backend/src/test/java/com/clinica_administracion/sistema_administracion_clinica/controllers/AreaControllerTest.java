fb290c62eeb9a66876e325ce3f02770c3afb4bd6
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-02 15:41:10 -0300

    create area controller test, initialize

---
+++ backend/src/test/java/com/clinica_administracion/sistema_administracion_clinica/controllers/AreaControllerTest.java
@@ -0,0 +1,11 @@
+package com.clinica_administracion.sistema_administracion_clinica.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(controllers = AreaController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class AreaControllerTest {
+  
+}
