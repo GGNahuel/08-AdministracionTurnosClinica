793ba1572c2827d72dd029b35383d75c5f96c8bf
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-19 14:49:53 -0300

    extraction of postForm

--- frontend/src/hooks/PacienteRequests.tsx
+++ frontend/src/hooks/PacienteRequests.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { GetResponseType } from "../types/APIResponses";
+import { GetResponseType, MessageInterface, ReturnResponseType } from "../types/APIResponses";
 import { API_PREFIX } from "../constants/VariablesEntorno";
+import { Paciente } from "../types/Entities";
 
 export function useGetAllPacienteRequest() {
-  const [getResponse, setGetResponse] = useState<GetResponseType>()
+  const [getResponse, setGetResponse] = useState<GetResponseType | null>(null)
 
   useEffect(() => {
     async function generateData() {
@@ -16,4 +17,43 @@ export function useGetAllPacienteRequest() {
   }, [])
 
   return getResponse
+}
+
+export function usePostPaciente() {
+  const [returnedPost, setReturnedPost] = useState<ReturnResponseType>({
+    message: {} as MessageInterface, returnValue: {}
+  })
+
+  const sendPacienteToPost = async (ev: React.FormEvent<HTMLFormElement>) => {
+    ev.preventDefault()
+    const $form = ev.currentTarget
+    const formData = new FormData($form)
+
+    const pacienteToSend: Paciente = {
+      nombreCompleto: formData.get('nombreCompleto') as string,
+      dni: formData.get('dni') as string,
+      numeroContacto: Number(formData.get('numeroContacto') as string),
+      obraSocial: formData.get('obraSocial') as string,
+      turnos: []
+    };
+
+    const request = await fetch(API_PREFIX + "/paciente", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(pacienteToSend)
+    })
+    const returned : ReturnResponseType = await request.json()
+    setReturnedPost({
+      message: {
+        text: returned.message.text,
+        messageType: returned.message.messageType,
+        exceptionCause: returned.message.exceptionCause
+      },
+      returnValue: returned.returnValue
+    })
+  }
+
+  return {returnedPost, sendPacienteToPost}
 }
