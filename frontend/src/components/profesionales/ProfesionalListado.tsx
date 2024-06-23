71227bcffef93de7e46ffafbcfc13efbffc15dd6
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-23 18:52:33 -0300

    creación listado de profesionales

---
+++ frontend/src/components/profesionales/ProfesionalListado.tsx
@@ -0,0 +1,25 @@
+import { useGetAllPacientes } from "../../hooks/PacienteRequests";
+import { ProfesionalMed } from "../../types/Entities";
+
+export function ProfesionalListado() {
+  const data = useGetAllPacientes()
+  const results = data?.results as ProfesionalMed[]
+
+  return (
+    <section>
+      <h2>Listado de profesionales</h2>
+      {results?.map(profesional => (
+        <article>
+          <strong>{profesional.nombreCompleto}</strong>
+          <p>Dni: {profesional.dni}</p>
+          <p>Especialidades: 
+            <ul>{profesional.areas?.map(area => <li>{area}</li>)}</ul>
+          </p>
+          <p>Matricula: {profesional.numMatricula}</p>
+          <p>Teléfono: {profesional.numeroContacto}</p>
+          <p>Consultorio: {profesional.consultorio || "No asignado"}</p>
+        </article>
+      ))}
+    </section>
+  )
+}
