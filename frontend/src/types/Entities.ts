f2b1e92a2fe39097b86b8225377b53d1721761af
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-23 13:36:34 -0300

    added profesional and area type, changed paciente&profesional dni to dto in turno type

--- frontend/src/types/Entities.ts
+++ frontend/src/types/Entities.ts
@@ -1,15 +1,15 @@
 export type Turno = {
   id?: string,
-  pacienteDni: string,
+  pacienteDto: Paciente,
   fecha: string,
   horario: string,
   areaProfesional: string,
-  obraSocial: string,
+  obraSocial?: string,
   metodoDeAbono?: string
   estadoPago: string,
   comentario?: string,
   consultorio: number,
-  profesionalDni: string,
+  profesionalDto: ProfesionalMed,
   activo?: boolean
 }
 
@@ -20,4 +20,22 @@ export type Paciente = {
   numeroContacto: number,
   obraSocial?: string,
   turnos?: string[]
+}
+
+export type ProfesionalMed = {
+  id?: string,
+  nombreCompleto: string,
+  dni: string,
+  numeroContacto: number,
+  areas: string[],
+  numMatricula: number,
+  horarios?: string[],
+  consultorio?: number
+}
+
+export type AreaProfesional = {
+  id?: string,
+  nombre: string,
+  activa?: boolean,
+  necesitaTurno: boolean
 }
