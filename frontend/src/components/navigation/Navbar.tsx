3e6da6d6220b7708472eabeb55fe5e93af3c98e3
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-23 13:38:57 -0300

    navbar

--- frontend/src/components/navigation/Navbar.tsx
+++ frontend/src/components/navigation/Navbar.tsx
@@ -1,9 +1,11 @@
 function NavItem({ summary, items } : { summary: string, items: string[]}) {
   return (
     <li>
-      <details>
+      <details name="navItem">
         <summary>{summary}</summary>
-        {items.map(item => <span key={item}>{item}</span>)}
+        <ul>
+          {items.map(item => <li key={item}>{item}</li>)}
+        </ul>
       </details>
     </li>
   )
@@ -17,9 +19,10 @@ export function Navbar() {
         <h2>Clínica Salud</h2>
       </header>
       <ul>
-        <NavItem summary="Turnos" items={["Ver turnos", "Crear turno", "Buscar turno"]}/>
-        <NavItem summary="Paciente" items={["Registrar paciente", "Editar datos de paciente", "Buscar paciente"]}/>
-        <NavItem summary="Profesional médico" items={["Registrar profesional", "Editar datos de profesional", "Buscar profesional"]}/>
+        <NavItem summary="Turnos" items={["Ver turnos del día", "Crear turno", "Modificar turno", "Buscar turnos"]}/>
+        <NavItem summary="Paciente" items={["Registrar paciente", "Editar datos de paciente", "Buscar pacientes"]}/>
+        <NavItem summary="Profesional médico" items={["Registrar profesional", "Editar datos de profesional", "Buscar perfil del profesional"]}/>
+        <NavItem summary="Consultorios y áreas" items={["Ver consultorios y áreas"]}/>
       </ul>
     </nav>
   )
