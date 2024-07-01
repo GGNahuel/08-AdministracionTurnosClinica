03c5c050e1a22217db1e8ad11f70a9b05cb156ef
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-06-30 22:08:26 -0300

    replace paciente listado for turno creacion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { Navbar } from "./components/navigation/Navbar"
-import { PacienteListado } from "./components/pacientes/PacienteListado"
 import { TurnoListado } from "./components/turnos/TurnoListado"
+import { TurnoCreacion } from "./components/turnos/TurnoCreacion"
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
-      <PacienteListado></PacienteListado>
       <TurnoListado />
+      <TurnoCreacion />
     </>
   )
 }
