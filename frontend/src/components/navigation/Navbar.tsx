ea11037abb5db194c411764281203f29fb724732
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-07-10 20:25:34 -0300

    navbar styles

--- frontend/src/components/navigation/Navbar.tsx
+++ frontend/src/components/navigation/Navbar.tsx
@@ -2,7 +2,7 @@ function NavItem({ summary, items } : { summary: string, items: string[]}) {
   return (
     <li>
       <details name="navItem">
-        <summary>{summary}</summary>
+        <summary><h3>{summary}</h3></summary>
         <ul>
           {items.map(item => <li key={item}>{item}</li>)}
         </ul>
@@ -13,16 +13,21 @@ function NavItem({ summary, items } : { summary: string, items: string[]}) {
 
 export function Navbar() {
   return (
-    <nav>
+    <nav id="mainNavbar">
       <header>
         <img src="asd" alt="Logo de la clinica" />
-        <h2>Clínica Salud</h2>
+        <h2>Nombre clinica</h2>
       </header>
-      <ul>
+      <ul className="linkList">
         <NavItem summary="Turnos" items={["Ver turnos del día", "Crear turno", "Modificar turno", "Buscar turnos"]}/>
         <NavItem summary="Paciente" items={["Registrar paciente", "Editar datos de paciente", "Buscar pacientes"]}/>
         <NavItem summary="Profesional médico" items={["Registrar profesional", "Editar datos de profesional", "Buscar perfil del profesional"]}/>
         <NavItem summary="Consultorios y áreas" items={["Ver consultorios y áreas"]}/>
+      </ul>
+      <ul className="buttonList">
+        <li><button className="navButton">⚙️</button></li>
+        <li><button className="navButton">🚹</button></li>
+        <li><button className="navButton">🌐</button></li>
       </ul>
     </nav>
   )
