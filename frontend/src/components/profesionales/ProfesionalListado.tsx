e7d3af5c8ac891edc8a134c186e5fc643ae95c74
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-07-25 22:09:52 -0300

    add key prop to profesional listado mapping

--- frontend/src/components/profesionales/ProfesionalListado.tsx
+++ frontend/src/components/profesionales/ProfesionalListado.tsx
@@ -8,18 +8,32 @@ export function ProfesionalListado() {
   return (
     <section>
       <h2>Listado de profesionales</h2>
-      {results?.map(profesional => (
-        <article>
-          <strong>{profesional.nombreCompleto}</strong>
-          <p>Dni: {profesional.dni}</p>
-          <p>Especialidades: 
-            <ul>{profesional.areas?.map(area => <li>{area}</li>)}</ul>
-          </p>
-          <p>Matricula: {profesional.numMatricula}</p>
-          <p>Teléfono: {profesional.numeroContacto}</p>
-          <p>Consultorio: {profesional.consultorio || "No asignado"}</p>
-        </article>
-      ))}
+      <table>
+        <thead><tr>
+            <th>Nombre completo</th>
+            <th>DNI</th>
+            <th>Especialidades</th>
+            <th>Matricula</th>
+            <th>Telefono</th>
+            <th>Consultorio</th>
+            <th>Horarios de atención</th>
+        </tr></thead>
+        <tbody>
+          {results?.map(profesional => (
+            <tr key={profesional.id}>
+              <td>{profesional.nombreCompleto}</td>
+              <td>{profesional.dni}</td>
+              <td>
+                <ul>{profesional.areas?.map(area => <li>{area}</li>)}</ul>
+              </td>
+              <td>{profesional.numMatricula}</td>
+              <td>{profesional.numeroContacto}</td>
+              <td>{profesional.consultorio || "No asignado"}</td>
+              <td>horarios</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
   )
 }
