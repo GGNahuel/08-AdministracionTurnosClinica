87f4dfab466847950e400e1033aaaa9184e1f43f
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-07-30 12:48:55 -0300

    small changes for better ui in area&cons list element

--- frontend/src/components/area_consultorio/AreaConsultorioList.tsx
+++ frontend/src/components/area_consultorio/AreaConsultorioList.tsx
@@ -1,21 +1,38 @@
 import { useGetAllAreas } from "../../hooks/AreaRequests";
 import { useGetAllConsultorios } from "../../hooks/ConsultorioRequests";
-import { AreaProfesional, Consultorio } from "../../types/Entities";
+import { useGetAllProfesionales } from "../../hooks/ProfesionalRequests";
+import { AreaProfesional, Consultorio, ProfesionalMed } from "../../types/Entities";
 
 export function AreaConsList() {
   const areas = useGetAllAreas()?.results as AreaProfesional[]
   const consultorios = useGetAllConsultorios()?.results as Consultorio[]
+  const profesionales = useGetAllProfesionales()?.results as ProfesionalMed[]
 
   return (
     <section>
       <h1>Listados</h1>
       <section>
         <h2>Consultorios existentes</h2>
-        <div>
-          {consultorios?.map(consultorio => (
-            <p key={consultorio.numeroConsultorio}>{consultorio.numeroConsultorio}</p>
-          ))}
-        </div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Número</th>
+              <th>Profesional asignado</th>
+            </tr>
+          </thead>
+          <tbody>
+            {consultorios?.map(consultorio => {
+              const profesionalAsignado = profesionales?.find(profesional => profesional.consultorio == consultorio.numeroConsultorio)
+
+              return(
+                <tr key={consultorio.numeroConsultorio}>
+                  <td className="center">{consultorio.numeroConsultorio}</td>
+                  <td>{profesionalAsignado ? profesionalAsignado.nombreCompleto : "No definido"}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </section>
       <section>
         <h2>Áreas profesionales</h2>
@@ -29,7 +46,7 @@ export function AreaConsList() {
           </thead>
           <tbody>
             {areas?.map(area => (
-              <tr key={area.nombre}>
+              <tr key={area.nombre} className={area.activa ? "" : "inactive"}>
                 <td>{area.nombre}</td>
                 <td className="center">{area.necesitaTurno ? "Con turnos" : "Por orden de llegada"}</td>
                 <td className="center">{area.activa ? "Activa" : "Inactiva"}</td>
