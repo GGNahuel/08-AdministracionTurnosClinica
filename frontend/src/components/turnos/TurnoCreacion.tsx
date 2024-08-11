bfd7430cffbd6cef09de993f32bc034d904a0a47
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-08-11 17:06:34 -0300

    minor changes in styles and required inputs create turno

--- frontend/src/components/turnos/TurnoCreacion.tsx
+++ frontend/src/components/turnos/TurnoCreacion.tsx
@@ -4,12 +4,13 @@ import { usePostTurno } from "../../hooks/TurnoRequests";
 import { AreaProfesional, Paciente, ProfesionalMed } from "../../types/Entities";
 import { useGetProfesionalsByArea } from "../../hooks/ProfesionalRequests";
 import { useGetPacientesByName } from "../../hooks/PacienteRequests";
+import Message from "../utilities/Message";
 
 export function TurnoCreacion() {
   const [areaSelected, setAreaSelected] = useState<string>("")
   const [searchPaciente, setSearchPaciente] = useState<string>("")
 
-  const {sendData} = usePostTurno()
+  const {returnedPost, sendData} = usePostTurno()
   const allAreas = useGetAllAreas()?.results as AreaProfesional[]
   const pacientesList = useGetPacientesByName(searchPaciente)?.results as Paciente[]
 
@@ -18,12 +19,13 @@ export function TurnoCreacion() {
   return (
     <section className="registerSection">
       <h1>Registrar turno</h1>
+      {returnedPost?.message.text && <Message messageObject={returnedPost.message}/>}
       <form id="turnoForm" onSubmit={(ev) => {
         sendData(ev)
       }}>
         <label>
           Servicio: 
-          <select name="area" onChange={(ev) => setAreaSelected(ev.target.value)}>
+          <select name="area" onChange={(ev) => setAreaSelected(ev.target.value)} required>
             <option value={""}>Seleccione un área</option>
             {allAreas.map(area => (
               <option key={area.nombre} value={area.nombre}>{area.nombre}</option>
@@ -37,7 +39,7 @@ export function TurnoCreacion() {
               profesionalesByAreas?.map(profesional => (
                 <option key={profesional.dni} value={profesional.dni}>{profesional.nombreCompleto}</option>
               )) :
-              <option>Seleccione un área para ver los profesionales disponibles</option>
+              <option value={""}>Seleccione un área para ver los profesionales disponibles</option>
             }
           </select>
         </label>
@@ -45,7 +47,7 @@ export function TurnoCreacion() {
           Nombre del paciente: (Ingrese el nombre y seleccione de la lista)
           <div className="grid autoColumns">
             <input type="search" onChange={(ev) => setSearchPaciente(ev.target.value)}/>
-            <select name="paciente">
+            <select name="paciente" required>
               {pacientesList?.length == 0 && <option value={""}>Ingrese el nombre para seleccionar el paciente</option>}
               {pacientesList?.map(paciente => (
                 <option key={paciente.dni} value={paciente.dni}>{paciente.nombreCompleto}</option>
