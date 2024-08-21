21cc171cb744ed2d9052b8f562bec492001ff9fa
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-08-21 00:09:16 -0300

    extraction of TableOptions function and component

--- frontend/src/components/area_consultorio/AreaConsultorioList.tsx
+++ frontend/src/components/area_consultorio/AreaConsultorioList.tsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
 import { useGetAllAreas } from "../../hooks/AreaRequests";
 import { useGetAllConsultorios } from "../../hooks/ConsultorioRequests";
 import { useGetAllProfesionales } from "../../hooks/ProfesionalRequests";
 import { useSelectedCheckboxesObject } from "../../hooks/SelectChecboxes";
-import { AreaProfesional, Consultorio, Entities, ProfesionalMed } from "../../types/Entities";
+import { AreaProfesional, Consultorio, ProfesionalMed } from "../../types/Entities";
 import { SelectItemCheckbox } from "../utilities/ListSelector";
-import { FatherCheckboxes } from "../../types/Others";
-import { concatArrays } from "../../functions/Utilities";
+
+import { useTableOptions } from "../utilities/TableOptions";
 
 export function AreaConsList() {
   const areas = useGetAllAreas()?.results as AreaProfesional[]
@@ -14,57 +13,14 @@ export function AreaConsList() {
   const profesionales = useGetAllProfesionales()?.results as ProfesionalMed[]
 
   const selectCheckboxesState = useSelectedCheckboxesObject()
-
-  /* para las acciones al seleccionar. Funcion en el elemento de la seccion que contiene la tabla, es función se ejecutará al hacer click en un input,
-  es decir se ejecuta en el selectItemCheckbox (función que recibe como parametro). Esta función le va a pasar a un estado las entidades seleccionadas,
-  otro componente recibirá/ tendrá ese estado y se encargará de aplicar las funciones correspondientes
-  */
-  const [selectedEntities, setSelectedEntities] = useState<Record<FatherCheckboxes, Entities[]>>({
-    turnos: [],
-    pacientes: [],
-    profesionales: [],
-    consultorios: [],
-    areas: []
-  })
-  const selectedEntitiesFunction = (params : {entityType: FatherCheckboxes, inputChecked: boolean, entity?: Entities, entities?: Entities[]}) => {
-    const {entityType, inputChecked, entity, entities} = params
-
-    if ((entity == undefined && entities == undefined) || (entity != undefined && entities != undefined)) 
-      throw new Error("La función debe recibir o una entidad o una lista de entidades")
-
-    let updatedEntities: Entities[] = []
-
-    if (inputChecked) {
-      if (entity != undefined) {
-        const temporalArray: Entities[] = []
-        temporalArray.push(entity)
-        updatedEntities = concatArrays(selectedEntities[entityType], temporalArray) as Entities[]
-      } else if (entities != undefined) {
-        entities.forEach(entitySelected => {
-          updatedEntities.push(entitySelected)
-        })
-      }
-    } else {
-      updatedEntities = entity != undefined ? selectedEntities[entityType].filter(entitySelected => entitySelected.id != entity.id) : []
-    }
-
-    setSelectedEntities(prev => ({
-        ...prev,
-        [entityType]: updatedEntities 
-      })
-    )
-  }
+  const {selectedEntitiesFunction, component: TableOptions} = useTableOptions()
 
   return (
     <section>
       <h1>Listados</h1>
       <section>
         <h2>Consultorios existentes</h2>
-        <nav>
-          <button disabled={selectedEntities.consultorios.length != 1}>Editar</button>
-          <button disabled={selectedEntities.consultorios.length == 0}>Dar de baja</button>
-          <button disabled={selectedEntities.consultorios.length == 0}>Eliminar</button>
-        </nav>
+        <TableOptions entityType="consultorios" />
         <table className="table">
           <thead>
             <tr>
@@ -102,6 +58,7 @@ export function AreaConsList() {
       </section>
       <section>
         <h2>Áreas profesionales</h2>
+        <TableOptions entityType="areas" />
         <table className="table">
           <thead>
             <tr>
---
+++ frontend/src/components/utilities/TableOptions.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import { concatArrays } from "../../functions/Utilities"
+import { Entities } from "../../types/Entities"
+import { FatherCheckboxes } from "../../types/Others"
+
+export function useTableOptions() {
+  const [selectedEntities, setSelectedEntities] = useState<Record<FatherCheckboxes, Entities[]>>({
+    turnos: [],
+    pacientes: [],
+    profesionales: [],
+    consultorios: [],
+    areas: []
+  })
+
+  const selectedEntitiesFunction = (params : {entityType: FatherCheckboxes, inputChecked: boolean, entity?: Entities, entities?: Entities[]}) => {
+    const {entityType, inputChecked, entity, entities} = params
+
+    if ((entity == undefined && entities == undefined) || (entity != undefined && entities != undefined)) 
+      throw new Error("La función debe recibir o una entidad o una lista de entidades")
+
+    let updatedEntities: Entities[] = []
+
+    if (inputChecked) {
+      if (entity != undefined) {
+        const temporalArray: Entities[] = []
+        temporalArray.push(entity)
+        updatedEntities = concatArrays(selectedEntities[entityType], temporalArray) as Entities[]
+      } else if (entities != undefined) {
+        entities.forEach(entitySelected => {
+          updatedEntities.push(entitySelected)
+        })
+      }
+    } else {
+      updatedEntities = entity != undefined ? selectedEntities[entityType].filter(entitySelected => entitySelected.id != entity.id) : []
+    }
+
+    setSelectedEntities(prev => ({
+        ...prev,
+        [entityType]: updatedEntities 
+      })
+    )
+  }
+
+  const component = (props : {entityType: FatherCheckboxes}) => {
+    const {entityType} = props
+    
+    return (
+      <nav>
+        <button disabled={selectedEntities[entityType].length != 1}>Editar</button>
+        <button disabled={selectedEntities[entityType].length == 0}>Dar de baja</button>
+        <button disabled={selectedEntities[entityType].length == 0}>Eliminar</button>
+      </nav>
+    )
+  }
+
+  return {component, selectedEntitiesFunction}
+}
