2fa631500cb2b5c1e2e36918d764b9151b6d7263
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-08-26 12:55:59 -0300

    join table navbar to tables in paciente and profesional views

--- frontend/src/components/pacientes/PacienteListado.tsx
+++ frontend/src/components/pacientes/PacienteListado.tsx
@@ -33,74 +33,76 @@ export function PacienteListado() {
     <>
       <section>
         <h2>Listado de pacientes</h2>
-        <TableOptions entityType="pacientes" selectedCheckboxesState={selectCheckboxesState} childs={results}/>
-        <table className="table">
-          <thead>
-            <tr>
-              <th className="checkbox"></th>
-              <th>Nombre del paciente</th>
-              <th>Dni</th>
-              <th>Número de contacto</th>
-              <th>Obra social</th>
-              <th>Turnos</th>
-            </tr>
-          </thead>
-          <tbody>
-            {results?.map((paciente) => {
-              return (
-                <React.Fragment key={paciente.id}>
-                  <tr className="list paciente">
-                    <td className="checkbox">
-                      <SelectItemCheckbox 
-                        selectedCheckboxesObject={selectCheckboxesState} 
-                        fatherName="pacientes" fatherOrChild="child" 
-                        child={paciente} markSelectedEntitiesFunction={selectedEntitiesFunction}
-                      />
-                    </td>
-                    <td>{paciente.nombreCompleto}</td>
-                    <td>{paciente.dni}</td>
-                    <td>{paciente.numeroContacto}</td>
-                    <td>{paciente.obraSocial || "Sin obra social"}</td>
-                    <td onClick={() => showTurnos(paciente.dni)}>
-                      <p>Ver turnos: </p>
-                    </td>
-                  </tr>
-                  {pacienteSelectedByDni.dni === paciente.dni && 
-                    (pacienteSelectedByDni.turnos.length > 0 ? 
-                      pacienteSelectedByDni.turnos?.map((turno) => (
-                        <tr className="list turno">
+        <section>  
+          <TableOptions entityType="pacientes" selectedCheckboxesState={selectCheckboxesState} childs={results}/>
+          <table className="table">
+            <thead>
+              <tr>
+                <th className="checkbox"></th>
+                <th>Nombre del paciente</th>
+                <th>Dni</th>
+                <th>Número de contacto</th>
+                <th>Obra social</th>
+                <th>Turnos</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results?.map((paciente) => {
+                return (
+                  <React.Fragment key={paciente.id}>
+                    <tr className="list paciente">
+                      <td className="checkbox">
+                        <SelectItemCheckbox 
+                          selectedCheckboxesObject={selectCheckboxesState} 
+                          fatherName="pacientes" fatherOrChild="child" 
+                          child={paciente} markSelectedEntitiesFunction={selectedEntitiesFunction}
+                        />
+                      </td>
+                      <td>{paciente.nombreCompleto}</td>
+                      <td>{paciente.dni}</td>
+                      <td>{paciente.numeroContacto}</td>
+                      <td>{paciente.obraSocial || "Sin obra social"}</td>
+                      <td onClick={() => showTurnos(paciente.dni)}>
+                        <p>Ver turnos: </p>
+                      </td>
+                    </tr>
+                    {pacienteSelectedByDni.dni === paciente.dni && 
+                      (pacienteSelectedByDni.turnos.length > 0 ? 
+                        pacienteSelectedByDni.turnos?.map((turno) => (
+                          <tr className="list turno">
+                            <td colSpan={6}>
+                              <table>
+                                <thead>
+                                  <tr>
+                                    <th>Fecha</th>
+                                    <th>Horario</th>
+                                    <th>Area profesional</th>
+                                  </tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td>{turno.fecha} - {turno.fecha}</td>
+                                    <td>{turno.horario}:{turno.horario}</td>
+                                    <td>{turno.areaProfesional}</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                            </td>
+                          </tr>
+                        )) :
+                        <tr>
                           <td colSpan={6}>
-                            <table>
-                              <thead>
-                                <tr>
-                                  <th>Fecha</th>
-                                  <th>Horario</th>
-                                  <th>Area profesional</th>
-                                </tr>
-                              </thead>
-                              <tbody>
-                                <tr>
-                                  <td>{turno.fecha} - {turno.fecha}</td>
-                                  <td>{turno.horario}:{turno.horario}</td>
-                                  <td>{turno.areaProfesional}</td>
-                                </tr>
-                              </tbody>
-                            </table>
+                            No hay turnos asociados al paciente
                           </td>
                         </tr>
-                      )) :
-                      <tr>
-                        <td colSpan={6}>
-                          No hay turnos asociados al paciente
-                        </td>
-                      </tr>
-                    )
-                  }
-                </React.Fragment>
-              )
-            })}
-          </tbody>
-        </table>
+                      )
+                    }
+                  </React.Fragment>
+                )
+              })}
+            </tbody>
+          </table>
+        </section>
       </section>
     </>
   )
--- frontend/src/components/profesionales/ProfesionalListado.tsx
+++ frontend/src/components/profesionales/ProfesionalListado.tsx
@@ -15,52 +15,54 @@ export function ProfesionalListado() {
   return (
     <section>
       <h1>Listado de profesionales</h1>
-      <TableOptions entityType="profesionales" selectedCheckboxesState={selectedCheckboxesObject} childs={results}/>
-      <table className="table">
-        <thead><tr>
-            <th>
-              <SelectItemCheckbox 
-                selectedCheckboxesObject={selectedCheckboxesObject}
-                fatherName="profesionales" fatherOrChild="father"
-                childElements={results} markSelectedEntitiesFunction={selectedEntitiesFunction}
-              />
-            </th>
-            <th>Nombre completo</th>
-            <th>DNI</th>
-            <th>Especialidades</th>
-            <th>Matricula</th>
-            <th>Telefono</th>
-            <th>Consultorio</th>
-            <th>Horarios de atención</th>
-        </tr></thead>
-        <tbody>
-          {results?.map(profesional => {
-            const { horarios } = profesional
-            const formattedHorariosList = horarios && Horario.getScheduleBlocksFromStrings(horarios)
-            
-            return(
-              <tr key={profesional.id}>
-                <td className="checkbox">
-                  <SelectItemCheckbox
-                    selectedCheckboxesObject={selectedCheckboxesObject}
-                    fatherName="profesionales" fatherOrChild="child"
-                    child={profesional} markSelectedEntitiesFunction={selectedEntitiesFunction}
-                  />
-                </td>
-                <td>{profesional.nombreCompleto}</td>
-                <td>{profesional.dni}</td>
-                <td className="center">
-                  <ul>{profesional.areas?.map(area => <li key={area}>{area}</li>)}</ul>
-                </td>
-                <td className="right">{profesional.numMatricula}</td>
-                <td className="right">{profesional.numeroContacto}</td>
-                <td className="right">{profesional.consultorio || "No asignado"}</td>
-                <td className="center">{formattedHorariosList}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
+      <section>
+        <TableOptions entityType="profesionales" selectedCheckboxesState={selectedCheckboxesObject} childs={results}/>
+        <table className="table">
+          <thead><tr>
+              <th>
+                <SelectItemCheckbox 
+                  selectedCheckboxesObject={selectedCheckboxesObject}
+                  fatherName="profesionales" fatherOrChild="father"
+                  childElements={results} markSelectedEntitiesFunction={selectedEntitiesFunction}
+                />
+              </th>
+              <th>Nombre completo</th>
+              <th>DNI</th>
+              <th>Especialidades</th>
+              <th>Matricula</th>
+              <th>Telefono</th>
+              <th>Consultorio</th>
+              <th>Horarios de atención</th>
+          </tr></thead>
+          <tbody>
+            {results?.map(profesional => {
+              const { horarios } = profesional
+              const formattedHorariosList = horarios && Horario.getScheduleBlocksFromStrings(horarios)
+              
+              return(
+                <tr key={profesional.id}>
+                  <td className="checkbox">
+                    <SelectItemCheckbox
+                      selectedCheckboxesObject={selectedCheckboxesObject}
+                      fatherName="profesionales" fatherOrChild="child"
+                      child={profesional} markSelectedEntitiesFunction={selectedEntitiesFunction}
+                    />
+                  </td>
+                  <td>{profesional.nombreCompleto}</td>
+                  <td>{profesional.dni}</td>
+                  <td className="center">
+                    <ul>{profesional.areas?.map(area => <li key={area}>{area}</li>)}</ul>
+                  </td>
+                  <td className="right">{profesional.numMatricula}</td>
+                  <td className="right">{profesional.numeroContacto}</td>
+                  <td className="right">{profesional.consultorio || "No asignado"}</td>
+                  <td className="center">{formattedHorariosList}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
     </section>
   )
 }
