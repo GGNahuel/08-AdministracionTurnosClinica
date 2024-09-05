1d3a16b19d7fd5f23d7e232ac471271b678dc5c9
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-09-04 22:23:51 -0300

    change the icons in navbar to svg and added style to these buttons

--- frontend/src/components/navigation/Navbar.tsx
+++ frontend/src/components/navigation/Navbar.tsx
@@ -2,6 +2,10 @@ import { Link } from "react-router-dom"
 import { navListItems } from "../../constants/NavigationComponents"
 import { NavbarDetails } from "../../types/NavbarSections"
 
+import configIcon from "../../assets/configSvg.svg"
+import globalIcon from "../../assets/globalSvg.svg"
+import logInIcon from "../../assets/logInSvg.svg"
+
 function NavItem({ navItem } : { navItem: NavbarDetails, route: string}) {
   const itemsNames : string[] = Object.values(navItem.items).map(linkObj => linkObj.name)
   const itemsRoutes = Object.values(navItem.items).map(linkObj => linkObj.route)
@@ -38,9 +42,9 @@ export function Navbar() {
       </section>
       <footer>
         <ul className="buttonList">
-          <li><button className="navButton">⚙️</button></li>
-          <li><button className="navButton">🚹</button></li>
-          <li><button className="navButton">🌐</button></li>
+          <li><button className="iconButton"><img src={configIcon} alt="" className="icon"/></button></li>
+          <li><button className="iconButton"><img src={logInIcon} alt="" className="icon"/></button></li>
+          <li><button className="iconButton"><img src={globalIcon} alt="" className="icon"/></button></li>
         </ul>
       </footer>
     </nav>
