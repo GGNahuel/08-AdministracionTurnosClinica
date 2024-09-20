fb4e25294d407fb4fbb57240ee48badc7ccd415d
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-09-20 14:41:43 -0300

    creation of search interfaces

--- frontend/src/types/SearchFormTypes.ts
+++ frontend/src/types/SearchFormTypes.ts
@@ -1,8 +1,30 @@
 import { EstadoPago } from "./BackendEnums";
 
+export type SearchTypes = SearchTurno | SearchProffesional | SearchPaciente | SearchArea
+
 export interface SearchTurno {
   searchName: string,
   areaName: string,
   estadoPago: typeof EstadoPago[number],
   date: string
+}
+
+export interface SearchProffesional {
+  search: string,
+  matricula: string,
+  area: string
+}
+
+export interface SearchPaciente {
+  search: string,
+  obraSocial: string
+}
+
+export interface SearchConsultorio {
+
+}
+
+export interface SearchArea {
+  status: string, 
+  schedule: string
 }
