53edb5d1eaece0b7ff8c1e90b672db3e1507516b
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-09-27 13:41:51 -0300

    login & registration forms

---
+++ frontend/src/components/users/LogAndRegister.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent, useState } from "react"
+import { useGetAllProfesionales } from "../../requests/ProfesionalRequests"
+import { ProfesionalMed } from "../../types/Entities"
+
+export function LogInForm() {
+  return (
+    <section className="registerSection">
+      <form>
+
+      </form>
+    </section>
+  )
+}
+
+export function RegisterForm() {
+  const [isProffesional, setIsProffesional] = useState(false)
+  const proffesionals = useGetAllProfesionales()?.results as ProfesionalMed[]
+
+  const handleIsProffesionalOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.currentTarget.checked
+    setIsProffesional(newValue)
+  }
+
+  return (
+    <section className="registerSection">
+      <form>
+        <label>Nombre de usuario<input type="text" name="username" /></label>
+        <label>Contraseña<input type="password" name="password" /></label>
+        <label>Repetir contraseña<input type="password" name="password2" /></label>
+        <label>Correo electrónico<input type="email" name="email" /></label>
+        <div>
+          <label>
+            <input type="checkbox" name="isProffesional" checked={isProffesional} onChange={e => handleIsProffesionalOnChange(e)}/>
+            Marque si usted es un profesional de salud
+          </label>
+          {isProffesional && 
+            <select name="proffesionalDNI">
+              {proffesionals?.map(proffesional => <option value={proffesional.dni}>{proffesional.nombreCompleto}</option>)}
+            </select>
+          }
+        </div>
+        <button type="submit">Registrar</button>
+      </form>
+    </section>
+  )
+}
