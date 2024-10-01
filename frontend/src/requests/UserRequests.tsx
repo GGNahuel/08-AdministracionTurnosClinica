c6c687c30c979798f2dd026cfb04b10fc2e9153a
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-09-30 21:20:52 -0300

    login request

--- frontend/src/requests/UserRequests.tsx
+++ frontend/src/requests/UserRequests.tsx
@@ -1,7 +1,8 @@
 import { FormEvent, useEffect, useState } from "react";
 import { HandledResponse, ReturnResponseType } from "../types/APIResponses";
 import { UserRegistration } from "../types/Entities";
-import { handleRequest } from "../functions/RequestHandler";
+import { getCookie, handleRequest } from "../functions/RequestHandler";
+import { useNavigate } from "react-router-dom";
 
 export function useRegisterUser() {
   const [response, setResponse] = useState<HandledResponse<ReturnResponseType>>()
@@ -31,9 +32,30 @@ export function useRegisterUser() {
 }
 
 export function useLogIn() {
+  const navigateTo = useNavigate()
+
   const sendLogInData = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
+    const username = formData.get("username") as string
+    const password = formData.get("password") as string
 
+    fetch("/logincheck", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "X-XSRF-TOKEN" : getCookie("XSRF-TOKEN")
+      },
+      body: new URLSearchParams({
+        username: username,
+        contrasena: password, // Usa el nombre del parámetro que definiste
+      }),
+      credentials: "include"
+    }).then(response => {
+      if (response.ok) navigateTo("/")
+      else console.log("loguin error: " + response.body)
+    })
   }
+
+  return {sendLogInData}
 }
