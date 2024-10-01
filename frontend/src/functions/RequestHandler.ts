3ecfdc4f9e6e11330e3a9602d7f81b96a63b0ff9
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2024-09-30 21:21:19 -0300

    check if the cookie exist before adding the csrf header

--- frontend/src/functions/RequestHandler.ts
+++ frontend/src/functions/RequestHandler.ts
@@ -15,42 +15,41 @@ export async function handleRequest(
   const requestBody: RequestInit = {
     method: method,
     headers: {
-      ...headers,
-      "X-XSRF-TOKEN" : getCookie("XSRF-TOKEN")
+      ...headers
     },
     body: alternativeBody ? alternativeBody : body ? JSON.stringify(body) : undefined,
     credentials: "include"
   }
+
   if (method != "GET" && !headers?.["Content-Type"])
     requestBody.headers = {...requestBody.headers, "Content-Type": "application/json"}
-  /* if (getCookie("XSRF-TOKEN") != "") {
+  if (getCookie("XSRF-TOKEN") != "") {
     requestBody.headers = {
       ...requestBody.headers,
       "X-XSRF-TOKEN" : getCookie("XSRF-TOKEN")
     }
-  }  */
+  }
 
   const request = await fetch(API_PREFIX + path, requestBody)
   const status = request.status
   const response: ResponseType = await request.json()
-  console.log(request, response, requestBody)
-
+  
   if (response.message != null && response.message.messageType == "warn") {
     // en lugar del console error iria algo para registrar ese error en algún lugar
     console.warn(response.message.text)
-
+    
     throw json({
       text: "Ocurrió un error inesperado en el servidor, vuelva a intentarlo más tarde"
     }, {status: 500})
   }
-
+  
+  console.log([request, {...response, status}, requestBody])
   return {...response, status} as HandledResponse<ResponseType>
 }
 
-function getCookie(name: string) : string {
+export function getCookie(name: string) : string {
   const value = `; ${document.cookie}`
   const parts = value.split(`; ${name}=`)
-  console.log(value, parts, document.cookie)
   // const check = parts.pop()?.split(';').shift()
   return parts.length === 2 ? parts[1] : ""
 }
