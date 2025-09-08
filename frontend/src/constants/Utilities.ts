158445c2f6873bbc19186a3852543db4fb10177d
Nahuel Gómez Gahn <nahuelg8799@gmail.com>
2025-09-08 10:19:05 -0300

    add api prefix as an env variable

--- frontend/src/constants/Utilities.ts
+++ frontend/src/constants/Utilities.ts
@@ -1,4 +1,4 @@
-export const API_PREFIX = "http://localhost:8080/api"
+export const API_PREFIX = import.meta.env.VITE_API_PREFIX
 export const DATE_FORMAT = /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/
 export const DATE_INPUT_FORMAT = /[0-9]{4}-[0-9]{2}-[0-9]{2}/
 export const SCHEDULE_FORMAT = /[0-2][0-9]:[0-5][0-9]/
